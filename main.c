5906631f98a47a7c76069d248192efba026b0459
Jiří Kerner <jirikernerjr@gmail.com>
2026-06-09 20:54:07 +0200

    Recursive backtracking

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <raylib.h>
 #include <math.h>
 
-#define WIDTH 840
-#define HEIGHT 840
+#define SCREEN_WIDTH 840
+#define SCREEN_HEIGHT 840
 #define CELL_COUNT 100
+#define CELL_SIZE 40
+#define GRID_WIDTH 10
+#define GRID_HEIGHT 10
 
 typedef struct
 {
@@ -12,123 +15,149 @@ typedef struct
     bool visited;
 } Cell;
 
+int stack_index = 0;
+Cell stack[CELL_COUNT];
+bool walls_broken[CELL_COUNT][4];
+
 void create_cells(Cell *cells)
 {
-    int cell_size = 40;
-    float pos_x = 40;
-    float pos_y = 40;
-
-    for (int i = 0; i <= CELL_COUNT; i++)
+    for (int i = 0; i < CELL_COUNT; i++)
     {
-        if (i > 0)
-        {
-            pos_x += 2 * cell_size;
-        }
+        int col = i % GRID_WIDTH;
+        int row = i / GRID_HEIGHT;
+        float pos_x = CELL_SIZE + col * 2 * CELL_SIZE;
+        float pos_y = CELL_SIZE + row * 2 * CELL_SIZE;
 
         cells[i].pos = (Vector2){pos_x, pos_y};
         cells[i].visited = false;
 
-        DrawRectangle(pos_x, pos_y, cell_size, cell_size, WHITE);
-
-        if (i > 0 && i % (CELL_COUNT / 10) == 0)
+        // Initialize wall tracking
+        for (int j = 0; j < 4; j++)
         {
-            pos_x = -cell_size;
-            pos_y += 80;
+            walls_broken[i][j] = false;
         }
     }
 }
 
-void break_wall(Cell *cells, int start_index, int random_nb, bool breaked)
+void draw_maze(Cell *cells)
 {
-    int stack_index = 0;
-    Cell stack[CELL_COUNT];
-
-    stack[stack_index] = cells[start_index];
-    cells[start_index].visited = true;
-
-    int cell_size = 40;
-
-    float start_pos_x = stack[0].pos.x;
-    float start_pos_y = stack[0].pos.y;
-
-    float nb_pos_x = 0;
-    float nb_pos_y = 0;
-    int wall_pos_x = 0;
-    int wall_pos_y = 0;
-
-    // Select a neighbouring cell
-    switch (random_nb)
+    // Draw all cells
+    for (int i = 0; i < CELL_COUNT; i++)
     {
-    case 1:
-        nb_pos_x = start_pos_x + 2 * cell_size;
-        nb_pos_y = start_pos_y;
-        wall_pos_x = -cell_size;
-        break;
-    case 2:
-        nb_pos_x = start_pos_x - 2 * cell_size;
-        nb_pos_y = start_pos_y;
-        wall_pos_x = cell_size;
-        break;
-    case 3:
-        nb_pos_x = start_pos_x;
-        nb_pos_y = start_pos_y + 2 * cell_size;
-        wall_pos_y = -cell_size;
-        break;
-    case 4:
-        nb_pos_x = start_pos_x;
-        nb_pos_y = start_pos_y - 2 * cell_size;
-        wall_pos_y = cell_size;
-        break;
+        DrawRectangle(cells[i].pos.x, cells[i].pos.y, CELL_SIZE, CELL_SIZE, WHITE);
     }
 
-    for (int i = 0; !breaked; i++)
+    // Redraw broken walls
+    for (int i = 0; i < CELL_COUNT; i++)
     {
-        if (nb_pos_x <= WIDTH - cell_size && nb_pos_y <= HEIGHT - cell_size)
+        float start_pos_x = cells[i].pos.x;
+        float start_pos_y = cells[i].pos.y;
+
+        // Check next column position
+        if (walls_broken[i][0])
         {
-            float cur_pos_x = cells[i].pos.x;
-            float cur_pos_y = cells[i].pos.y;
+            DrawRectangle(start_pos_x + CELL_SIZE, start_pos_y, CELL_SIZE, CELL_SIZE, WHITE);
+        }
 
-            // Remove the wall between cells
-            if (cur_pos_x == nb_pos_x && cur_pos_y == nb_pos_y)
-            {
-                DrawRectangle(nb_pos_x + wall_pos_x, nb_pos_y + wall_pos_y, cell_size, cell_size, WHITE);
+        // Check next row position
+        if (walls_broken[i][1])
+        {
+            DrawRectangle(start_pos_x, start_pos_y + CELL_SIZE, CELL_SIZE, CELL_SIZE, WHITE);
+        }
+    }
+}
 
-                stack[stack_index] = cells[i];
-                cells[i].visited = true;
-                stack_index++;
+void break_wall(Cell *cells, int current_index)
+{
+    stack[stack_index++] = cells[current_index];
+    cells[current_index].visited = true;
 
-                breaked = true;
-            }
+    int col = current_index % GRID_WIDTH;
+    int row = current_index / GRID_HEIGHT;
+
+    int neighbors[4] = {-1, -1, -1, -1};
+    int directions[4] = {-1, -1, -1, -1};
+    int neighbor_count = 0;
+
+    // Check neighboring cells
+    if (col < GRID_WIDTH - 1)
+    {
+        neighbors[neighbor_count] = current_index + 1;
+        directions[neighbor_count] = 0; // right
+        neighbor_count++;
+    }
+
+    if (row < GRID_HEIGHT - 1)
+    {
+        neighbors[neighbor_count] = current_index + GRID_WIDTH;
+        directions[neighbor_count] = 1; // bottom
+        neighbor_count++;
+    }
+
+    if (col > 0)
+    {
+        neighbors[neighbor_count] = current_index - 1;
+        directions[neighbor_count] = 2; // left
+        neighbor_count++;
+    }
+
+    if (row > 0)
+    {
+        neighbors[neighbor_count] = current_index - GRID_WIDTH;
+        directions[neighbor_count] = 3; // top
+        neighbor_count++;
+    }
+
+    // Try each unvisited neighbor
+    while (neighbor_count > 0)
+    {
+        int random_nb = GetRandomValue(0, neighbor_count - 1);
+        int next_index = neighbors[random_nb];
+        int direction = directions[random_nb];
+
+        if (next_index >= 0 && !cells[next_index].visited)
+        {
+            // Mark the wall as broken
+            walls_broken[current_index][direction] = true;
+
+            int opposite_direction = (direction + 2) % 4;
+            walls_broken[next_index][opposite_direction] = true;
+
+            break_wall(cells, next_index);
         }
         else
         {
-            break;
+            neighbor_count--;
+
+            // Remove this neighbor from the list and try again
+            neighbors[random_nb] = neighbors[neighbor_count];
+            directions[random_nb] = directions[neighbor_count];
         }
     }
+
+    // Backtrack after dead end
+    stack_index--;
 }
 
 int main()
 {
-    InitWindow(WIDTH, HEIGHT, "Maze Generator");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Maze Generator");
 
     SetTargetFPS(60);
 
     Cell cells[CELL_COUNT];
 
-    int start_index = GetRandomValue(1, CELL_COUNT);
+    create_cells(&cells[0]);
 
-    int min_nb = 1;
-    int max_nb = 4;
-    int random_nb = GetRandomValue(min_nb, max_nb);
-    bool wall_breaked = false;
+    int start_index = GetRandomValue(0, CELL_COUNT - 1);
+    break_wall(&cells[0], start_index);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(BLACK);
 
-        create_cells(&cells[0]);
-
-        break_wall(&cells[0], start_index, random_nb, wall_breaked);
+        draw_maze(&cells[0]);
 
         EndDrawing();
     }
